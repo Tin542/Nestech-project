3fbb66d7544cafcf13e68584b440b171df7b64ed
TramDB <tinnt0504@gmail.com>
2023-04-27 00:22:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express') // import express
+const app = express() // khai bao
+const CONFIG = require('./config');
+const mongoose = require('mongoose');
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello bin')
+})
+
+app.listen(CONFIG.PORT, () => {
+  console.log(`Example app listening on port ${CONFIG.PORT}`)
+})
+
+// tao repo ==> readme.md: họ tên mem, mongo url, invite
+// vô mongo atlas tạo db ==> database ==> network access: tạo ip 
+// tạo trello ==> workspace ==> invite
+
+// view (trang chủ)
+// models
+
