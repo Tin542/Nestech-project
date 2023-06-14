80c36a9c04047add2229be8283e4f22f1b415028
ThuyDung <thuydunghothi1110@gmail.com>
2023-06-14 18:51:05 +0700

    email server

--- services/emailService.js
+++ services/emailService.js
@@ -6,13 +6,13 @@ function EmailService() {
       return new Promise((resolve, reject) => {
         // set up api key
         sgMail.setApiKey(
-          "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"
+          "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"
         );
 
         // set up for email
         const msg = {
           to: emailCustomer, // email người nhận
-          from: "ndhieuvegia983@gmail.com", // email người gửi
+          from: "thuydunghothi1110@gmail.com", // email người gửi
           subject: "Mã OTP để đăng kí tài khoản",// tiêu đề email
           // html: `` // nội dung email(style)
           text: `Mã OTP của bạn là ${otp}  =(^,^)=`, // nội dung email (text)
