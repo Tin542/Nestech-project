c2603459db7487d2ce5508fb123e26a13edb30ce
NhatAnh01 <lequangnhatanhhv0112@gmail.com>
2023-07-01 16:28:58 +0700

    category

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const adminController = require('../controllers/adminController');
 const authController = require('../controllers/authController');
+const categoryControler = require('../controllers/categoryControler');
 const router = express.Router({});
 const fileService = require("../services/fileService");
 const multer = require("multer");
@@ -33,7 +34,12 @@ router.get('/user/list', adminController.users);
 router.get('/staffs/list', adminController.staffs);
 router.post('/staffs/add', adminController.addStaff);
 router.get("/staffs/export", adminController.exportStaff);
-
+// Category
+router.get('/category/list', categoryControler.getList);
+router.post('/category/add', categoryControler.addcategory);
+router.get('/category/detail/:id', categoryControler.getcategorydetail);
+router.post('/category/edit', categoryControler.editcategory);
+router.delete('/category/delete/:id', categoryControler.deletecategory);
 // Excel import, export
 // router.get("/file", (req, res) => {
 //     res.render("pages/testList");
